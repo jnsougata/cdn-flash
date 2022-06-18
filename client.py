88043e02b412c8190ccc43cbc92435419319c9fe
Sougata Jana <dwipjana293@gmail.com>
2022-06-18 06:46:54 +0530

    Update client.py

--- client.py
+++ client.py
@@ -6,12 +6,12 @@ import asyncio
 async def main():
     async with aiohttp.ClientSession() as session:
         headers = {
-            'DETA-PROJECT-KEY': os.getenv('DETA_TOKEN'),
-            'DRIVE-NAME': 'PixeL_@11223344',
-            'FILE-NAME': 'covers/default_card.png'
+            'DETA-PROJECT-KEY': ...'),
+            'DRIVE-NAME': ...,
+            'FILE-NAME': ...'
         }
         async with session.post('https://cdn-flash.herokuapp.com/cdn', headers=headers) as resp:
-            print(await resp.read())
+            print(await resp.json())
             await session.close()
 
 if __name__ == '__main__':
