013e561aecbb06a70cba14240673382a47fe2137
jnsougata <dwipjana293@gmail.com>
2022-06-18 05:48:59 +0530

    changes & bug fixes

--- main.py
+++ main.py
@@ -29,11 +29,11 @@ async def make_cdn(request: web.Request):
     await deta.connect()
     drive = deta.drive(drive_name)
     file = await drive.get(file_name)
-    local_name = file.name.split('/')[-1]
+    local_name = file_name.split('/')[-1]
     with open(local_name, 'wb') as f:
         f.write(await file.read())
     await deta.close()
-    return web.json_response({'url': f'https://cdn-flash.herokuapp.com/file/{file_name}'})
+    return web.json_response({'url': f'https://cdn-flash.herokuapp.com/file/{local_name}'})
 
 
 @routes.get('/file/{file_name}')
